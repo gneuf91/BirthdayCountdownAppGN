7a80a24679ebae3e13bb00a94c9418947332c3cb
Gaven Neufeld <gavensmac@Gavens-MacBook-Pro.local>
2018-04-04 14:35:31 -0600

    Added custom colors and heading styles

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -8,6 +8,7 @@ class Clock extends Component {
             this.timer = 0
             this.birthday = props.birthdayFormState.startDate.toString();
             this.getTimeRemaining = this.getTimeRemaining.bind(this);
+            this.noBirthYear = new Date(this.birthday).getFullYear() == new Date().getFullYear();
 
             this.state = {
                 timeRemaining: this.getTimeRemaining(this.props.birthdayFormState.startDate.toString())
@@ -79,6 +80,17 @@ class Clock extends Component {
             clearInterval(this.timer);
         }
 
+        renderMessage = function() {
+            if(this.noBirthYear) {
+                return (
+                    <h4>until your birthday!</h4>
+                )
+            }
+            return (
+                <h4>Remaining until you are {this.getAge()}</h4>
+            )
+        }.bind(this)
+
         render() {
             const data = this.state.timeRemaining
             return (
@@ -95,7 +107,7 @@ class Clock extends Component {
                             <div>SECS {data.seconds}</div>
                         </div>
                         <div>
-                            {<h4>Remaining until you are {this.getAge()}</h4>}
+                            {this.renderMessage()}
                         </div>
                     </div>
                 }  
